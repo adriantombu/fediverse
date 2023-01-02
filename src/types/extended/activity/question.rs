007a2d04da9b1bf2c6db1fa02e2af51e318f6c53
Adrian Tombu <adrian@otso.fr>
2023-01-02 19:05:16 +0100

    ✨ Add extended activity objects

--- src/types/extended/activity/question.rs
+++ src/types/extended/activity/question.rs
@@ -1,1 +1,26 @@
-pub struct Question {}
+use crate::types::properties::actor::Actor;
+use crate::types::properties::instrument::Instrument;
+use crate::types::properties::object::Object;
+use crate::types::properties::origin::Origin;
+use crate::types::properties::result::Result;
+use crate::types::properties::target::Target;
+
+/// Represents a question being asked.
+///
+/// Question objects are an extension of [IntransitiveActivity](crate::types::core::intransitive_activity::IntransitiveActivity).
+/// That is, the Question object is an Activity, but the direct object is the question itself and
+/// therefore it would not contain an [object](crate::types::properties::object::Object) property.
+///
+/// Either of the [anyOf](crate::types::properties::any_of::AnyOf) and [oneOf](crate::types::properties::one_of::OneOf)
+/// properties MAY be used to express possible answers, but a Question object MUST NOT have both properties.
+///
+/// Specifications: <https://www.w3.org/TR/activitystreams-vocabulary/#dfn-question>
+pub struct Question {
+    // Properties from Activity
+    pub actor: Option<Actor>,
+    pub object: Option<Object>,
+    pub target: Option<Target>,
+    pub result: Option<Result>,
+    pub origin: Option<Origin>,
+    pub instrument: Option<Instrument>,
+}
--- src/types/extended/activity/travel.rs
+++ src/types/extended/activity/travel.rs
@@ -1,1 +1,24 @@
-pub struct Travel {}
+use crate::types::properties::actor::Actor;
+use crate::types::properties::instrument::Instrument;
+use crate::types::properties::object::Object;
+use crate::types::properties::origin::Origin;
+use crate::types::properties::result::Result;
+use crate::types::properties::target::Target;
+
+/// Indicates that the `actor` is traveling to `target` from `origin`.
+///
+/// Travel is an [IntransitiveObject](crate::types::core::intransitive_activity::IntransitiveActivity)
+/// whose actor specifies the direct object.
+///
+/// If the `target` or `origin` are not specified, either can be determined by context.
+///
+/// Specifications: <https://www.w3.org/TR/activitystreams-vocabulary/#dfn-travel>
+pub struct Travel {
+    // Properties from Activity
+    pub actor: Option<Actor>,
+    pub object: Option<Object>,
+    pub target: Option<Target>,
+    pub result: Option<Result>,
+    pub origin: Option<Origin>,
+    pub instrument: Option<Instrument>,
+}
